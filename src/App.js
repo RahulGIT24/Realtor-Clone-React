7c9c8c3c682001b10d5e5e470382321936e0bdf5
Rahul <rg4005450@gmail.com>
2023-05-17 10:12:37 +0530

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <h1 className="text-red-600 text-6xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  );
+}
+
+export default App;
