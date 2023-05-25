285392eb414aab1c60fac8c6803c882044fa8f63
Rahul <rg4005450@gmail.com>
2023-05-25 20:46:50 +0530

    Added comments in Private route

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,15 +1,23 @@
+// Imports from React
 import React from "react";
+
+// Imports from react router dom
 import { Outlet, Navigate } from "react-router-dom";
-import {useAuthStatus} from "../hooks/useAuthStatus";
-import Spinner from "../Components/Spinner"
+
+// Importing custom hook
+import { useAuthStatus } from "../hooks/useAuthStatus";
+
+// Importing Spinner component
+import Spinner from "../Components/Spinner";
 
 function PrivateRoute() {
+  // Getting it from custom hook
   const { loggedin, checkingStatus } = useAuthStatus();
-    if(checkingStatus){
-        return (
-            <Spinner/>
-        )
-    }
+  // If checkingStatus is true returns a spinner
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+  // If loggedin is true then render Outlet otherwise navigate to sign in
   return loggedin ? <Outlet></Outlet> : <Navigate to="/sign-in/" />;
 }
 
