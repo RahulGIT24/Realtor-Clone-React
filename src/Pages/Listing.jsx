85c7ee1a590c902c67409f32cd89bfec7aafa340
Rahul <rg4005450@gmail.com>
2023-05-26 17:59:06 +0530

    Share link functionality updated

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -11,10 +11,12 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { MdFileCopy } from "react-icons/md";
 
 function Listing() {
   const [loading, setLoading] = useState(true);
   const [listing, setListing] = useState(null);
+  const [linkCopied, setLinkCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   const params = useParams();
 
@@ -36,7 +38,7 @@ function Listing() {
   }
   return (
     <main>
-       <Swiper
+      <Swiper
         slidesPerView={1}
         navigation
         pagination={{ type: "progressbar" }}
@@ -56,6 +58,23 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setLinkCopied(true);
+          setTimeout(() => {
+            setLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <MdFileCopy className="text-lg text-slate-500" />
+      </div>
+      {linkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
