cddf7231d54727e5b1ba9504b1b8df854080a3f2
Rahul <rg4005450@gmail.com>
2023-05-28 08:59:47 +0530

    Updated Gitignore and added secrets

---
+++ src/secrets.js
@@ -0,0 +1,11 @@
+const secrets = {
+    API_KEY: "AIzaSyBDn4mPLsJwhtcPy3Y58Rb1ycDxMhNpgD8",
+    AUTH_DOMAIN : "react-realtor-ab173.firebaseapp.com",
+    PROJECT_ID : "react-realtor-ab173",
+    STORAGE_BUCKET : "react-realtor-ab173.appspot.com",
+    MESSAGING_SENDER_ID : "911316065735",
+    APP_ID : "1:911316065735:web:09c47c7e10d63f0fadcde2",
+    MEASUREMENT_ID : "G-VH9S9772Q4"
+}
+
+export default secrets;
